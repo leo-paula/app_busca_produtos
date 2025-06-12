2945169e17d1e2fd2be547f6d6112b55f2913181
vlcp_millenio <vinicius@millenio.com.br>
2025-06-12 11:10:35 -0300

    .

--- Main.py
+++ Main.py
@@ -1,16 +1,5 @@
-import uvicorn
-import os
 from fastapi import FastAPI
 from Routes import router
 
 app = FastAPI()
 app.include_router(router)
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", default=8000))
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=port,
-        reload=True
-    )
